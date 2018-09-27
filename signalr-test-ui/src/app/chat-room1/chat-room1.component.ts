0a9d22ddac8506ab5ee4f30075f60063542fb281
Cristiano Matzum Guilloux da Silva <cristianofx@gmail.com>
2018-09-27 13:58:15 -0300

    Added initial logic for client connection

--- signalr-test-ui/src/app/chat-room1/chat-room1.component.ts
+++ signalr-test-ui/src/app/chat-room1/chat-room1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
 
 @Component({
   selector: 'app-chat-room1',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatRoom1Component implements OnInit {
 
+  public hubConnection: HubConnection;
+
   constructor() { }
 
   ngOnInit() {
+    //this.signalRConnection();
+  }
+
+  ngOnDestroy() {
+    //this.closeSignalRConnection();
+  }
+
+  private closeSignalRConnection() {
+    if(this.hubConnection){
+      this.hubConnection.onclose(() => { });
+      this.hubConnection.stop();
+      this.hubConnection.off("SendAll");
+    }
+  }
+
+  signalRConnection() {
+    if (!this.hubConnection) {
+      let builder = new HubConnectionBuilder();
+      console.log("creating hub connection");
+      // as per setup in the startup.cs
+      this.hubConnection = builder.withUrl('http://localhost:5000/hubs/echo').build();
+      //this.hubConnection = builder.withUrl('http://localhost:5000/hubs/echo').build();
+      // message coming from the server
+      this.hubConnection.on("SendAll", (message) => {
+        //message logic here
+      });
+    }
+    this.hubConnection.onclose(() => {
+      this.hubConnection.start();
+    })
+    // starting the connection
+    this.hubConnection.start().catch(() => {
+      console.log("Connection to host failed");
+    });
   }
 
 }
